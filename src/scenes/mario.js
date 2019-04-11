a52ced8a522c23975a3d0e3c3c99a344de205f5c
Brady Dowling <brady@amazon.com>
2019-04-11 09:00:08 -0400

    Mario moves left (and gets stuck)

--- src/scenes/mario.js
+++ src/scenes/mario.js
@@ -39,9 +39,11 @@ export default class MarioGame extends Phaser.Scene {
 
     this.physics.add.collider(this.mario, this.layer);
 
-    this.mario.sprite = this.add.sprite(50, 50, 'mario');
+    this.mario.sprite = this.physics.add.sprite(50, 50, 'mario');
     this.mario.sprite.setScale(0.47);
     this.mario.sprite.setOrigin(0.5, 0.5);
+    this.mario.sprite.setBounce(0);
+    this.mario.sprite.setCollideWorldBounds(true);
 
     this.anims.create({
       key: 'left',
@@ -55,6 +57,67 @@ export default class MarioGame extends Phaser.Scene {
       frameRate: 10,
       repeat: -1
     });
-    debugger;
+    this.anims.create({
+      key: 'wait',
+      frames: this.anims.generateFrameNumbers('mario', { frames: [0] }),
+      frameRate: 10,
+      repeat: -1
+    });
+
+    this.mario.sprite.body.fixedRotation = true;
+
+    this.cursors = this.input.keyboard.createCursorKeys();
+    this.jumpButton = this.input.keyboard.addKey(
+      Phaser.Input.Keyboard.KeyCodes.SPACE
+    );
+    this.runButton = this.input.keyboard.addKey(
+      Phaser.Input.Keyboard.KeyCodes.SHIFT
+    );
+  }
+
+  update() {
+    this.mario.doNothing = true;
+    if (this.cursors.left.isDown) {
+      //mario.sprite.body.acceleration.x = -120;
+      if (this.mario.direction != 'left') {
+        // FIXME: Need to turn the guy around somehow
+        // this.mario.sprite.scale.x *= -1;
+        this.mario.direction = 'left';
+      }
+      if (
+        this.mario.sprite.body.velocity.x == 0 ||
+        (this.mario.sprite.anims.currentAnim.key != 'left' &&
+          this.mario.sprite.body.onFloor())
+      ) {
+        this.mario.sprite.anims.play('left', true);
+      }
+
+      this.mario.sprite.body.velocity.x -= 5;
+      if (this.runButton.isDown) {
+        if (this.mario.sprite.body.velocity.x < -200) {
+          this.mario.sprite.body.velocity.x = -200;
+        }
+      } else {
+        if (this.mario.sprite.body.velocity.x < -120) {
+          this.mario.sprite.body.velocity.x = -120;
+        }
+      }
+      this.mario.doNothing = false;
+    }
+    if (this.mario.doNothing) {
+      if (this.mario.sprite.body.velocity.x > 10) {
+        //mario.sprite.body.acceleration.x = 10;
+        this.mario.sprite.body.velocity.x -= 10;
+      } else if (this.mario.sprite.body.velocity.x < -10) {
+        //mario.sprite.body.acceleration.x = -10;
+        this.mario.sprite.body.velocity.x += 10;
+      } else {
+        //mario.sprite.body.acceleration.x = 0;
+        this.mario.sprite.body.velocity.x = 0;
+      }
+      if (this.mario.sprite.body.onFloor()) {
+        this.mario.sprite.anims.play('wait', 20, true);
+      }
+    }
   }
 }
