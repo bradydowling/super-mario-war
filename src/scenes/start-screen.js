dccf8c47480cb670bf03a0c85ece80cf60160d63
Brady Dowling <brady@amazon.com>
2019-05-26 20:18:04 -0400

    Broke text on menu screen

--- src/scenes/start-screen.js
+++ src/scenes/start-screen.js
@@ -2,6 +2,7 @@ import Phaser from 'phaser/src/phaser.js';
 import menu_smw from '../assets/menu_smw.png';
 import menu_background from '../assets/menu_background.png';
 import menu_music from '../assets/sounds/menu/menu.ogg';
+import menu_font_large from '../assets/fonts/font_large.png';
 
 export default class sceneTemplate extends Phaser.Scene {
   constructor() {
@@ -9,12 +10,40 @@ export default class sceneTemplate extends Phaser.Scene {
 
     this.startButton;
     this.music;
+    this.menuFont;
   }
+
   preload() {
     this.load.image('smw', menu_smw);
     this.load.image('background', menu_background);
     this.load.audio('menu_music', menu_music);
+    this.load.image('menu_font_large', menu_font_large);
   }
+
+  setMenuText(text) {
+    this.menuFont.text = text;
+  }
+
+  setupFont(key) {
+    const config = {
+      image: key,
+      width: 31,
+      height: 25,
+      chars: Phaser.GameObjects.RetroFont.TEXT_SET1,
+      charsPerRow: 11,
+      spacing: { x: 1, y: 1 }
+    };
+
+    this.cache.bitmapFont.add(
+      key,
+      Phaser.GameObjects.RetroFont.Parse(this, config)
+    );
+
+    this.menuFont = this.add.bitmapText(0, 200, key, 'PHASER 3');
+
+    this.menuFont.setScale(3);
+  }
+
   create() {
     this.bg = this.add.image(0, 0, 'background').setOrigin(0, 0);
     this.smw_menu = this.add.image(
@@ -29,7 +58,12 @@ export default class sceneTemplate extends Phaser.Scene {
     this.startButton = this.input.keyboard.addKey(
       Phaser.Input.Keyboard.KeyCodes.ENTER
     );
+
+    // Move this stuff elsewhere
+    this.setupFont('menu_font_large');
+    this.setMenuText('Some new stuff');
   }
+
   update() {
     if (Phaser.Input.Keyboard.JustDown(this.startButton)) {
       this.registry.set('player1img', 'sprites/players/Bowser.png');
